fc445b377e0f511df494723f20c3102411c7dfce
jorpac <36743869+jorpac@users.noreply.github.com>
2022-01-14 09:24:34 +0000

    patient receiving messages, vibrating and opening modal

--- PatientScreen.tsx
+++ PatientScreen.tsx
@@ -26,77 +26,20 @@ import {
   BackHandler,
   Alert,
   Vibration,
-  Modal
+  Modal,
+  Pressable,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import MQTT from 'tsm-react-native-mqtt';
 
-
-function activateMQTT(topic, message, values, timestamp, id) {
-  MQTT.createClient({
-    uri: 'mqtt://test.mosquitto.org:1883',
-    clientId: id /*
-     clientId: 'Medphd1',
-     uri: 'mqtts://a2c1d7385b234cd6a8ae4d287bfd8857.s1.eu.hivemq.cloud:8884',
-     host: 'a2c1d7385b234cd6a8ae4d287bfd8857.s1.eu.hivemq.cloud',
-     port: 8884,
-     protocol: 'mqtts',
-     user: 'Medphd1',
-     pass: '123456.mM',*/,
-    automaticReconnect: true,
-  })
-    .then(function (client) {
-      console.log('MQTT client activated', Math.floor(Math.random() * 100));
-      client.on('closed', function () {
-        console.log('mqtt.event.closed');
-      });
-      client.on('error', function (msg) {
-        console.log('mqtt.event.error', msg);
-        if (msg.includes('32103')) {
-          client.connect();
-        }
-      });
-
-      client.on('message', function (msg) {
-        console.log(msg.data);
-        console.log('mqtt.event.message', msg);
-      });
-
-      client.on('connect', function () {
-        console.log('connected');
-        client.publish(
-          topic,
-          message +
-            'last values: ' +
-            values[0] +
-            ' ' +
-            values[1] +
-            ' ' +
-            values[2] +
-            ' ' +
-            timestamp +
-            ' ' +
-            id,
-          0,
-          true,
-          false,
-        );
-        client.subscribe(topic + '/' + id, 0);
-      });
-
-      client.connect();
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-  return 1;
-}
-
 const PatientScreen = ({navigation}) => {
   const isDarkMode = useColorScheme() === 'dark';
   const [listElements, setListElements] = useState([0]);
   const [loadedElement, setLoadedElement] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [message, setMessage] = useState('');
+
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     paddingTop: 2,
@@ -105,6 +48,70 @@ const PatientScreen = ({navigation}) => {
   let last_time = new Date().getTime();
   let i = 1;
   var tempList = [];
+
+  function activateMQTT(topic, message, values, timestamp, id) {
+    MQTT.createClient({
+      uri: 'mqtt://test.mosquitto.org:1883',
+      clientId: id /*
+     clientId: 'Medphd1',
+     uri: 'mqtts://a2c1d7385b234cd6a8ae4d287bfd8857.s1.eu.hivemq.cloud:8884',
+     host: 'a2c1d7385b234cd6a8ae4d287bfd8857.s1.eu.hivemq.cloud',
+     port: 8884,
+     protocol: 'mqtts',
+     user: 'Medphd1',
+     pass: '123456.mM',*/,
+      automaticReconnect: true,
+    })
+      .then(function (client) {
+        console.log('MQTT client activated', Math.floor(Math.random() * 100));
+        client.on('closed', function () {
+          console.log('mqtt.event.closed');
+        });
+        client.on('error', function (msg) {
+          console.log('mqtt.event.error', msg);
+          if (msg.includes('32103')) {
+            client.connect();
+          }
+        });
+
+        client.on('message', function (msg) {
+          console.log(msg.data);
+          console.log('mqtt.event.message', msg);
+          setMessage(msg.data);
+          setModalVisible(true);
+          Vibration.vibrate(1000);
+        });
+
+        client.on('connect', function () {
+          console.log('connected');
+          client.publish(
+            topic,
+            message +
+              'last values: ' +
+              values[0] +
+              ' ' +
+              values[1] +
+              ' ' +
+              values[2] +
+              ' ' +
+              timestamp +
+              ' ' +
+              id,
+            0,
+            true,
+            false,
+          );
+          client.subscribe(topic + '/' + id, 0);
+        });
+
+        client.connect();
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    return 1;
+  }
+
   function generate() {
     let new_time = new Date().getTime();
     i++;
@@ -182,6 +189,24 @@ const PatientScreen = ({navigation}) => {
           </TouchableOpacity>
         )}
       />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>{message}</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>OK!</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
@@ -215,6 +240,47 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#333',
     padding: 25,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 
